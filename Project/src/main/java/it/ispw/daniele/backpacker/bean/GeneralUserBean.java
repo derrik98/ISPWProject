c536b6bb85d3c5c8c1721725d219f69c2c894fcb
derrik98 <danielederrico98@gmail.com>
2022-10-31 00:26:21 +0100

    eliminazione primi code smell

--- Project/src/main/java/it/ispw/daniele/backpacker/bean/GeneralUserBean.java
+++ Project/src/main/java/it/ispw/daniele/backpacker/bean/GeneralUserBean.java
@@ -50,36 +50,4 @@ public class GeneralUserBean implements Serializable {
         return this.profilePicture;
     }
 
-//    private static GeneralUserBean INSTANCE = null;
-//
-//    public final GeneralUserBean getInstance() {
-//        if(INSTANCE==null) {
-//            INSTANCE = new GeneralUserBean();
-//        }
-//
-//        return INSTANCE;
-//    }
-//
-//    public GeneralUserBean() {
-//    }
-//
-//    public GeneralUserBean(String user, String pass) {
-//        this.username = user;
-//        this.password = pass;
-//    }
-
-
-//    public boolean validate() throws Exception {
-//        System.out.println("loginbean" + getLogged() + getEmail() + getPassword());
-//
-//        if (email == null || email.equals("") || password.equals(null) || password.equals("")) {
-//            return false;
-//        }
-//        System.out.println("LOGIN BEAN   " + email + password);
-//        LoginBean utenteTrovato = LoginController.getInstances().login(email, password);
-//        System.out.println(this.isLogged);
-//        return utenteTrovato != null;
-//
-//    }
-
 }
